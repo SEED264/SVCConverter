5c4ab82fdb22915f73a8c3f500c667a8f5b287bc
SEED264 <seedx264@gmail.com>
2020-08-17 09:36:00 +0900

    Add input recorder for register

--- src/input_recorder.cc
+++ src/input_recorder.cc
@@ -24,12 +24,52 @@ void ButtonRecorder::SetButtonStates(std::unordered_map<USHORT, std::unordered_m
 
 void KnobRecorder::SetKnobState(const HIDUsagePair &usage, const RawInputValue &state) {
     bool is_first_record = !current_states_.count(usage);
-    difference_[usage] = (is_first_record) ? 0
-                                           : state.value - current_states_[usage].value;
+    if (!is_first_record) {
+        auto previous_value = current_states_[usage].value;
+        long difference = state.value - previous_value;
+        auto range = state.max - state.min;
+        bool is_increase = difference > 0;
+        bool is_reverse = difference > (range / 2);
+        if (is_increase && is_reverse) {
+            difference = (state.value - state.max) + (state.min - previous_value);
+        } else if (is_increase && is_reverse) {
+            difference = (state.max - previous_value) + (state.value - state.min);
+        }
+        difference_[usage] = difference;
+    }
     current_states_[usage] = state;
+
 }
 
 void KnobRecorder::SetKnobStates(const std::map<HIDUsagePair, RawInputValue> &states) {
+    for (auto st = states.begin(); st != states.end(); ++st) {
+        auto usage = st->first;
+        auto state = st->second;
+        SetKnobState(usage, state);
+    }
+}
+
+void KnobRecorderForRegister::SetKnobState(const HIDUsagePair &usage, const RawInputValue &state) {
+    bool is_first_record = !current_states_.count(usage);
+    if (!is_first_record) {
+        auto previous_value = current_states_[usage].value;
+        long difference = state.value - previous_value;
+        auto range = state.max - state.min;
+        bool is_increase = difference > 0;
+        bool is_reverse = difference > (range / 2);
+        if (is_increase && is_reverse) {
+            difference = (state.value - state.max) + (state.min - previous_value);
+        } else if (is_increase && is_reverse) {
+            difference = (state.max - previous_value) + (state.value - state.min);
+        }
+        total_difference_[usage] += difference;
+        if (range)
+            total_difference_ratio_[usage] += static_cast<double>(difference) / range;
+    }
+    current_states_[usage] = state;
+}
+
+void KnobRecorderForRegister::SetKnobStates(const std::map<HIDUsagePair, RawInputValue> &states) {
     for (auto st = states.begin(); st != states.end(); ++st) {
         auto usage = st->first;
         auto state = st->second;
--- src/input_recorder.h
+++ src/input_recorder.h
@@ -3,14 +3,15 @@
 
 #include "input_manager.h"
 
+// Record button states and triggered, released flags
 class ButtonRecorder {
 public:
     void SetButtonStates(USHORT usage_page, std::unordered_map<USHORT, bool> states);
     void SetButtonStates(std::unordered_map<USHORT, std::unordered_map<USHORT, bool>> states);
 
-    auto GetTriggeredStates() { return triggered_buttons_; }
-    auto GetPressingStates() { return pressing_buttons_; }
-    auto GetReleasedStates() { return released_buttons_; }
+    auto GetTriggeredStates() const { return triggered_buttons_; }
+    auto GetPressingStates() const { return pressing_buttons_; }
+    auto GetReleasedStates() const { return released_buttons_; }
 
 private:
     std::unordered_map<USHORT, std::unordered_map<USHORT, bool>> triggered_buttons_;
@@ -18,17 +19,35 @@ private:
     std::unordered_map<USHORT, std::unordered_map<USHORT, bool>> released_buttons_;
 };
 
+// Recording knob states and difference
 class KnobRecorder {
 public:
     void SetKnobState(const HIDUsagePair &usage, const RawInputValue &state);
     void SetKnobStates(const std::map<HIDUsagePair, RawInputValue> &states);
 
-    auto GetCurrentStates() { return current_states_; }
-    auto GetDifference() { return difference_; }
+    auto GetCurrentStates() const { return current_states_; }
+    auto GetDifference() const { return difference_; }
 
 private:
     std::map<HIDUsagePair, RawInputValue> current_states_;
-    std::map<HIDUsagePair, unsigned long> difference_;
+    std::map<HIDUsagePair, long> difference_;
+};
+
+// Record knob states and total difference for register knob binding
+class KnobRecorderForRegister {
+public:
+    void SetKnobState(const HIDUsagePair &usage, const RawInputValue &state);
+    void SetKnobStates(const std::map<HIDUsagePair, RawInputValue> &states);
+
+    auto GetCurrentStates() const { return current_states_; }
+
+    auto GetTotalDifference() const { return total_difference_; }
+    auto GetTotalDifferenceAsRatio() const { return total_difference_ratio_; }
+
+private:
+    std::map<HIDUsagePair, long> total_difference_;
+    std::map<HIDUsagePair, double> total_difference_ratio_;
+    std::map<HIDUsagePair, RawInputValue> current_states_;
 };
 
 #endif // _SVCCONVERTER_SRC_INPUT_RECORDER_H_
