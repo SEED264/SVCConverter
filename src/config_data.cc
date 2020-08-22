f7e0be1e039e0d0a12bddaeed53f7fd2de413f88
SEED264 <seedx264@gmail.com>
2020-08-22 17:14:59 +0900

    Edit binding info

---
+++ src/config_data.cc
@@ -0,0 +1,5 @@
+#include "config_data.h"
+
+SVCControllBindProfile::SVCControllBindProfile(wxString profile_name_)
+        : profile_name(profile_name_) {
+}
--- src/config_data.h
+++ src/config_data.h
@@ -6,11 +6,16 @@
 #endif
 
 #include <limits>
+#include <map>
 #include <vector>
 #include <windows.h>
 extern "C" {
 #include <hidsdi.h>
 }
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif // WX_PRECOMP
 
 const unsigned long SVCUsageNotMapped = ULONG_MAX;
 
@@ -23,41 +28,45 @@ enum SVCKnobIncreaseDirection {
 // Information for binding the button to the key
 struct SVCButtonBindInfo {
     // RawInput device list
-    RAWINPUTDEVICELIST device_list;
+    RAWINPUTDEVICELIST device_list = { .hDevice = nullptr };
 
     // Usage page
-    unsigned long usage_page;
+    unsigned short usage_page = 0;
     // Usage id
-    unsigned long usage_id;
+    unsigned short usage_id = 0;
 
     // Key code
-    unsigned char key;
+    unsigned char key = 0;
 };
 
 // Information for binding the knob to the key
 struct SVCKnobBindInfo {
     // RawInput device list
-    RAWINPUTDEVICELIST device_list;
+    RAWINPUTDEVICELIST device_list = { .hDevice = nullptr };
 
     // Usage page
-    unsigned long usage_page;
+    unsigned short usage_page = 0;
     // Usage id
-    unsigned long usage_id;
+    unsigned short usage_id = 0;
 
     // Key code sent when the knob is rotates to the left
-    unsigned char l_key;
+    unsigned char l_key = 0;
     // Key code sent when the knob is rotates to the right
-    unsigned char r_key;
+    unsigned char r_key = 0;
 
     // Knob rotate direction when the value is increasing
-    SVCKnobIncreaseDirection increase_direction;
+    SVCKnobIncreaseDirection increase_direction = SVCKnobIncreaseDirection::Right;
 };
 
 // Controller binding info 
-struct SVCControllBindInfo {
+struct SVCControllBindProfile {
+    wxString profile_name;
     // Knobs
     SVCKnobBindInfo knob_l,
                     knob_r;
+
+    // START button
+    SVCButtonBindInfo start;
 
     // BT buttons
     SVCButtonBindInfo bt_a,
@@ -70,7 +79,9 @@ struct SVCControllBindInfo {
                       fx_r;
 
     // Additional button bindings
-    std::vector<SVCButtonBindInfo> extra_buttons;
+    std::map<wxString, SVCButtonBindInfo> extra_buttons;
+
+    SVCControllBindProfile(wxString profile_name_ = "default");
 };
 
 #endif // _SVCCONVERTER_SRC_CONFIG_DATA_H_
